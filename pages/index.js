9b0022e321e45243d118656f02013045c4f0ca17
pavelklos <pavelkloscz@gmail.com>
2021-04-15 23:04:48 +0200

    240. [06:05] Adding First Pages To The Project

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,86 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+// rafce
 
-export default function Home() {
+const HomePage = () => {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <section>
+      <h1>The Home Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit voluptate,
+        iusto repellendus similique animi aliquid magni corrupti eaque, eos
+        dicta ex eligendi. Animi, eveniet voluptatem. Voluptate architecto saepe
+        minima facilis natus nemo tempore voluptatem fuga quam laboriosam culpa
+        beatae similique voluptates eaque voluptatum modi, enim magnam sunt
+        atque odio ex eveniet! Blanditiis cumque dolorem deleniti quidem
+        expedita?
+      </p>
+    </section>
+  );
+};
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+export default HomePage;
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+// import Head from 'next/head'
+// import styles from '../styles/Home.module.css'
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+// export default function Home() {
+//   return (
+//     <div className={styles.container}>
+//       <Head>
+//         <title>Create Next App</title>
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+//       <main className={styles.main}>
+//         <h1 className={styles.title}>
+//           Welcome to <a href="https://nextjs.org">Next.js!</a>
+//         </h1>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+//         <p className={styles.description}>
+//           Get started by editing{' '}
+//           <code className={styles.code}>pages/index.js</code>
+//         </p>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+//         <div className={styles.grid}>
+//           <a href="https://nextjs.org/docs" className={styles.card}>
+//             <h3>Documentation &rarr;</h3>
+//             <p>Find in-depth information about Next.js features and API.</p>
+//           </a>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+//           <a href="https://nextjs.org/learn" className={styles.card}>
+//             <h3>Learn &rarr;</h3>
+//             <p>Learn about Next.js in an interactive course with quizzes!</p>
+//           </a>
+
+//           <a
+//             href="https://github.com/vercel/next.js/tree/master/examples"
+//             className={styles.card}
+//           >
+//             <h3>Examples &rarr;</h3>
+//             <p>Discover and deploy boilerplate example Next.js projects.</p>
+//           </a>
+
+//           <a
+//             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+//             className={styles.card}
+//           >
+//             <h3>Deploy &rarr;</h3>
+//             <p>
+//               Instantly deploy your Next.js site to a public URL with Vercel.
+//             </p>
+//           </a>
+//         </div>
+//       </main>
+
+//       <footer className={styles.footer}>
+//         <a
+//           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Powered by{' '}
+//           <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+//         </a>
+//       </footer>
+//     </div>
+//   )
+// }
---
+++ pages/news.js
@@ -0,0 +1,21 @@
+// rafce
+
+const NewsPage = () => {
+  return (
+    <section>
+      <h1>The News Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
+        maiores, ratione, necessitatibus pariatur explicabo voluptates vero
+        incidunt blanditiis sunt similique itaque rerum. Quisquam earum pariatur
+        dignissimos sapiente, eligendi similique veritatis tempore officiis
+        iusto corporis reprehenderit repudiandae culpa est alias nemo
+        perferendis excepturi expedita! Eum, at perferendis! Odit debitis
+        consectetur deleniti atque quia explicabo nihil excepturi possimus
+        porro.
+      </p>
+    </section>
+  );
+};
+
+export default NewsPage;
