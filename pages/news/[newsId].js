7238df2b61713aaf9e2a814c1c3b9723db269e4a
pavelklos <pavelkloscz@gmail.com>
2021-04-16 13:15:36 +0200

    243. [04:07] Extracting Dynamic Route Data

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,10 +1,20 @@
 // DOMAIN/news/[newsId]
 // rafce
+import { useRouter } from "next/router";
 
 const NewsDetailPage = () => {
+  const router = useRouter();
+  // const { query } = router;
+  // const { newsId } = query;
+  const newsId = router.query?.newsId;
+  // console.log({ router, query, newsId });
+
+  // send a request to the backend API (file system, database, ...)
+  // to fetch the news item with newsId
+
   return (
     <section>
-      <h1>The News Detail Page</h1>
+      <h1>The News Detail Page : {newsId}</h1>
       <p>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis odio
         qui voluptate nihil ducimus amet aliquam earum unde animi at illum
